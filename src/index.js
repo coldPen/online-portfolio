fcbf9615e6889c4a51819f9708105364767d471a
Lucas <lucas.leroux@protonmail.com>
2020-01-14 21:19:23 +0100

    fixed SVG bug on safari

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // Import SASS stylesheets
 import "./sass/main.scss";
+
+const projects = ["ateliers", "aouf", "wecashup"];
+
+projects.forEach(project =>
+  document.getElementById(project).addEventListener("click", e => {
+    console.log(e);
+  })
+);
