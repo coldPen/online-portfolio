9b8b2173a0f986713029ca4f19d322edace9b406
Lucas <lucas.leroux@protonmail.com>
2020-01-15 12:36:48 +0100

    building the project popup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ const projects = ["ateliers", "aouf", "wecashup"];
 
 projects.forEach(project =>
   document.getElementById(project).addEventListener("click", e => {
-    console.log(e);
+    // console.log(e.currentTarget.getBoundingClientRect());
+    const popup = document.createElement("div");
+    popup.classList.add("popup");
+    const content = Array.from(
+      document.getElementById(`${project}-content`).children
+    );
+    content.forEach(child => popup.appendChild(child));
+    const body = document.getElementById("body");
+    body.appendChild(popup);
   })
 );
