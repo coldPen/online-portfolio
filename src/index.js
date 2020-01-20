107b370e3d07eb6388e193255f78bc243ec08185
Lucas <lucas.leroux@protonmail.com>
2020-01-20 19:01:54 +0100

    WIP projects tiles animation | grid adjustments

--- src/index.js
+++ src/index.js
@@ -1,55 +1,75 @@
-// Import SASS stylesheets
+/* Import SASS stylesheets */
 import "./sass/main.scss";
 
-const styleSheet = document.styleSheets[0];
+/* Project tiles animations */
+(() => {
+  const styleSheet = document.styleSheets[0];
 
-// const lastSelectorIndex = Array.from(styleSheet.cssRules)
-//   .reverse()
-//   .findIndex(
-//     cssRule =>
-//       cssRule.selectorText && cssRule.selectorText.includes(`.projects__item`)
-//   );
-// const insertionIndex = styleSheet.cssRules.length - lastSelectorIndex;
+  const getInsertionIndex = selector =>
+    Array.from(styleSheet.cssRules).findIndex(
+      cssRule => cssRule.selectorText === selector
+    ) + 1;
 
-const insertionIndex =
-  Array.from(styleSheet.cssRules).findIndex(
-    cssRule => cssRule.selectorText === `.projects__item--selected`
-  ) + 1;
+  const insertionIndex_selectedItem = getInsertionIndex(
+    `.projects__item--selected`
+  );
 
-const projects = [`ateliers`, `aouf`, `wecashup`];
+  const insertionIndex_noScroll = getInsertionIndex(`.noScroll`);
 
-projects.forEach(project => {
-  const tile = document.getElementById(project);
+  const projects = [`ateliers`, `aouf`, `wecashup`];
 
-  tile.addEventListener(`click`, () => {
-    tile.blur();
+  projects.forEach(project => {
+    const tile = document.getElementById(project);
 
-    const { y: top, x: left, width, height } = tile.getBoundingClientRect();
+    tile.addEventListener(`mousedown`, e => e.preventDefault());
 
-    const posProperties = {
-      top: top + 2, // Because it's 2px too high on every browser for some reason
-      left,
-      width,
-      height
+    const openTile = () => {
+      const { y: top, x: left, width, height } = tile.getBoundingClientRect();
+
+      const itemPosProperties = {
+        top: top + 2, // Because it's 2px too high on every browser for some reason
+        left,
+        width,
+        height
+      };
+
+      const itemPosRules = Object.entries(itemPosProperties)
+        .map(([key, value]) => `${key}: ${value / 10}rem;`)
+        .join(` `);
+
+      styleSheet.insertRule(
+        `
+        .projects__item--selected {
+          ${itemPosRules}
+        }
+        `,
+        insertionIndex_selectedItem
+      );
+      tile.classList.add(`projects__item--selected`);
+
+      styleSheet.insertRule(
+        `
+        .noScroll {
+          top: -${window.scrollY / 10}rem;
+        }
+        `,
+        insertionIndex_noScroll
+      );
+      document.body.classList.add("noScroll");
+
+      setTimeout(() => {
+        tile.classList.add(`projects__item--active`);
+        setTimeout(() => {
+          tile.classList.remove(`projects__item--grid`);
+          tile.classList.add(`projects__item--full-screen`);
+        }, 400);
+      }, 100);
     };
 
-    const posRules = Object.entries(posProperties)
-      .map(([key, value]) => `${key}: ${value / 10}rem;`)
-      .join(` `);
+    tile.addEventListener(`keypress`, e => {
+      if (e.key === `Enter`) openTile();
+    });
 
-    styleSheet.insertRule(
-      `
-        .projects__item--selected {
-          ${posRules}
-        }
-      `,
-      insertionIndex
-    );
-    tile.classList.add(`projects__item--selected`);
-
-    document.body.style.top = `-${window.scrollY}px`;
-    document.body.classList.add("noScroll");
-
-    setTimeout(() => tile.classList.add(`projects__item--active`), 100);
+    tile.addEventListener(`click`, () => openTile());
   });
-});
+})();
