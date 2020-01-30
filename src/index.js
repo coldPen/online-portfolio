62a55ad32a6ff43196dd5980758d73e1c80e928d
Lucas Leroux <lucas.leroux@protonmail.com>
2020-01-30 18:12:57 +0100

    projects are now closable, transitions still wip

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./sass/main.scss";
 /* Project tiles animations */
 (() => {
   const styleSheet = document.styleSheets[0];
+  const { body } = document;
 
   const getStylesheetIndex = selector =>
-    Array.from(styleSheet.cssRules).findIndex(
-      cssRule => cssRule.selectorText === selector
-    ) + 1;
+    Array.from(styleSheet.cssRules).findIndex(cssRule => {
+      return cssRule.selectorText === selector;
+    }) + 1;
 
   const projects = [`ateliers`, `aouf`, `wecashup`];
 
@@ -18,6 +19,10 @@ import "./sass/main.scss";
     tile.addEventListener(`mousedown`, e => e.preventDefault());
 
     const openTile = () => {
+      tile.removeEventListener(`click`, openTile);
+      tile.removeEventListener(`keypress`, openTileOnEnter);
+
+      /* Open the tile */
       const { y: top, x: left, width, height } = tile.getBoundingClientRect();
 
       const itemPosProps = {
@@ -31,40 +36,73 @@ import "./sass/main.scss";
         .map(([key, value]) => `${key}: ${value / 10}rem;`)
         .join(` `);
 
+      const itemSelectedIndex = getStylesheetIndex(`.projects__item--selected`);
       styleSheet.insertRule(
         `
           .projects__item--selected {
             ${itemPosRules}
           }
         `,
-        getStylesheetIndex(`.projects__item--selected`)
+        itemSelectedIndex
       );
       tile.classList.add(`projects__item--selected`);
 
+      const noScrollIndex = getStylesheetIndex(`.no-scroll`);
+      const verticalOffset = window.pageYOffset;
       styleSheet.insertRule(
         `
           .no-scroll {
-            top: -${window.scrollY / 10}rem;
+            top: -${verticalOffset / 10}rem;
           }
         `,
-        getStylesheetIndex(`.no-scroll`)
+        noScrollIndex
       );
-      document.body.classList.add(`no-scroll`);
+      body.classList.add(`no-scroll`);
 
       setTimeout(() => {
         tile.classList.add(`projects__item--active`);
+
         setTimeout(() => {
           tile.classList.remove(`projects__item--grid`);
+          console.log(Array.from(tile.classList));
           tile.classList.add(`projects__item--full-screen`);
         }, 200);
       }, 4);
+
+      /* Make the tile closable with a button */
+      const closeButton = document.getElementById(`close-${project}`);
+
+      const closeProject = () => {
+        closeButton.removeEventListener(`click`, closeProject);
+
+        tile.classList.remove(`projects__item--full-screen`);
+        tile.classList.add(`projects__item--grid`);
+
+        setTimeout(() => {
+          tile.classList.remove(`projects__item--active`);
+
+          setTimeout(() => {
+            body.classList.remove(`no-scroll`);
+            styleSheet.deleteRule(noScrollIndex);
+            window.scrollBy(0, verticalOffset);
+
+            tile.classList.remove(`projects__item--selected`);
+            styleSheet.deleteRule(itemSelectedIndex);
+
+            tile.addEventListener(`click`, openTile);
+            tile.addEventListener(`keypress`, openTileOnEnter);
+          }, 4);
+        }, 200);
+      };
+
+      closeButton.addEventListener(`click`, closeProject);
     };
+    tile.addEventListener(`click`, openTile);
 
-    tile.addEventListener(`keypress`, e => {
-      if (e.key === `Enter`) openTile();
-    });
-
-    tile.addEventListener(`click`, () => openTile());
+    const openTileOnEnter = ({ key }) => {
+      if (key === `Enter`) openTile();
+    };
+    tile.addEventListener(`keypress`, openTileOnEnter);
   });
 })();
 
